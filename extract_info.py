61d5680312d086845a836fb1881d0c0f6f839889
Marlon Marín <marlonmariin97@outlook.com>
2023-01-03 23:09:22 -0500

    Final_project.py was complemented by filtering puntuactions in input file text and worked it appropiately in Coursera notebook.
    This script takes the key words 'h', 'min', and 'assignment' for returning an average time that all the learning process takes, per week and per total of the course.

---
+++ extract_info.py
@@ -0,0 +1,8 @@
+#This script is made for creating a dictionary for storing data about study time spent in Coursera courses
+
+def extract(week):
+    filtered_week = week.split()
+    print(filtered_week)
+        
+extract("Getting your Python on: 1h 15 min of videos, 40 min of reading, 1 graded assignment")
+
--- final_project.py
+++ final_project.py
@@ -3,23 +3,18 @@
 # Oh, and be sure to make it ignore word case, words that do not contain all alphabets 
 # and boring words like "and" or "the"
 
-# input_file = """"The concept of sustainability has become increasingly important in recent years, as concerns about the impact of human activity on the environment have grown. Sustainability refers to the ability to meet the needs of the present without compromising the ability of future generations to meet their own needs. It encompasses social, economic, and environmental factors, and requires the balancing of these elements in order to achieve a harmonious and stable system.
-# 
-# One key aspect of sustainability is the use of renewable resources. These are resources that can be replenished over time, such as solar and wind power, rather than finite resources like fossil fuels. The use of renewable resources helps to reduce the negative environmental impacts of resource extraction and helps to ensure that future generations will have access to the resources they need.
-# 
-# Another important aspect of sustainability is waste management. Proper waste management involves reducing the amount of waste produced, recycling as much as possible, and safely disposing of any remaining waste. This helps to conserve resources and prevent pollution.
-# 
-# Sustainability also involves considering the long-term effects of actions and decisions. This means taking into account not just the immediate costs and benefits, but also the potential long-term impacts on the environment and on future generations.
-# 
-# Overall, sustainability is a complex and multifaceted concept that requires a holistic approach to addressing the needs of both present and future generations. It is important for individuals, organizations, and governments to consider sustainability in their decision-making processes in order to create a more sustainable and equitable future for all."""
+input_file = """"The concept of sustainability has become increasingly important in recent years, as concerns about the impact of human activity on the environment have grown. Sustainability refers to the ability to meet the needs of the present without compromising the ability of future generations to meet their own needs. It encompasses social, economic, and environmental factors, and requires the balancing of these elements in order to achieve a harmonious and stable system.
 
-input_file = "¡Hey! I love the way you make me feel at ://123 street."
+One key aspect of sustainability is the use of renewable resources. These are resources that can be replenished over time, such as solar and wind power, rather than finite resources like fossil fuels. The use of renewable resources helps to reduce the negative environmental impacts of resource extraction and helps to ensure that future generations will have access to the resources they need.
+
+Another important aspect of sustainability is waste management. Proper waste management involves reducing the amount of waste produced, recycling as much as possible, and safely disposing of any remaining waste. This helps to conserve resources and prevent pollution.
+
+Sustainability also involves considering the long-term effects of actions and decisions. This means taking into account not just the immediate costs and benefits, but also the potential long-term impacts on the environment and on future generations.
+
+Overall, sustainability is a complex and multifaceted concept that requires a holistic approach to addressing the needs of both present and future generations. It is important for individuals, organizations, and governments to consider sustainability in their decision-making processes in order to create a more sustainable and equitable future for all."""
+
 
 def calculate_frequencies(file_contents):
-    file_contents_list = file_contents.lower().split(" ")
-    list_of_elements = []
-    n = 0
-    wordcloud_dict = {}
     punctuations = """¡!()-[]{};:'"\,<>./?@#$%^&*_~"""
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my", \
     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them", \
@@ -27,15 +22,22 @@ def calculate_frequencies(file_contents):
     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how", \
     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
     
-    for elements in file_contents_list:
-        if elements not in uninteresting_words:
-            list_of_elements.append(elements)
+    file_contents_list = []
+    word = ""
+    wordcloud_dict = {}
     
-    for element in list_of_elements:
-        for key in element:
-            if key.isalpha() and key not in punctuations:
-                pass
-    
-    
-        
+    for element in file_contents:
+        if element not in punctuations and element.isnumeric() == False:
+            word = element.lower()
+            file_contents_list.append(word)
+            
+    file_contents_list = "".join(file_contents_list).split()
+  
+    for items in file_contents_list:
+        if items not in uninteresting_words:
+            wordcloud_dict[items] = file_contents_list.count(items)
+    return wordcloud_dict
+            
+
+
 print(calculate_frequencies(input_file))
