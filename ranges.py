00b7254bd1dd774e2f79cff1c676d2d449ee5b2c
Marlon Marín <marlonmariin97@outlook.com>
2023-01-06 22:05:13 -0500

    Now, this script can receive lists of several manual estimations (i.e.: '<1/3' or '>= 3/4'). Errors and other aspects of avoiding non-valid entries have to be corrected. For instance, if an empty or not valid input is entered

--- ranges.py
+++ ranges.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
 
+
 def is_list(obj):
     if isinstance(obj, list):
         return True
@@ -7,41 +8,66 @@ def is_list(obj):
         return obj
     return False
 
+
 def get_ranges_list(input_list):
+    counter = 0
+    modified_list = []
+    
     for elements in input_list:
-        get_ranges_string(elements)
+        counter += 1
+        modified_list.append(get_ranges_string(
+            elements.replace(",", "").split()))
+        
+    counter = ["Container no. {}".format(x)
+               for x in range(1, len(modified_list)+1)]
+    modified_list = [(x, y) for x, y in zip(counter, modified_list)]
+    modified_dict = {key: value for key, value in modified_list}
+    
+    return modified_dict
+
 
 def get_ranges_string(input_string):
-    
+
     if is_list(input_string) == input_string:
         input_list = input_string.replace(",", "").split()
-    
+
+    elif is_list(input_string) == True:
+        input_list = []
+        for key in input_string:
+            input_list.append(key)
+
     for s in input_list:
+
         if "<" in input_list:
-            s = float(Fraction(input_list[1]))-0.1*float(Fraction(input_list[1]))
+            s = float(Fraction(input_list[1])) - \
+                0.1*float(Fraction(input_list[1]))
             s *= int(input_list[2])
-            return "Estimated quantity is {a:.2f} {b}".format(a = s, b = input_list[3])
-        
+            return "Estimated quantity is {a:.2f} {b}".format(a=s, b=input_list[3])
+
         elif "<=" in input_list:
-            s = float(Fraction(input_list[1]))-0.05*float(Fraction(input_list[1]))
+            s = float(Fraction(input_list[1])) - \
+                0.05*float(Fraction(input_list[1]))
             s *= int(input_list[2])
-            return "Estimated quantity is {a:.2f} {b}".format(a = s, b = input_list[3])
+            return "Estimated quantity is {a:.2f} {b}".format(a=s, b=input_list[3])
 
         elif "~" in input_list:
-            s = float(Fraction(input_list[1]))-0.01*float(Fraction(input_list[1]))
+            s = float(Fraction(input_list[1])) - \
+                0.01*float(Fraction(input_list[1]))
             s *= int(input_list[2])
-            return "Estimated quantity is {a:.2f} {b}".format(a = s, b = input_list[3])
+            return "Estimated quantity is {a:.2f} {b}".format(a=s, b=input_list[3])
 
         elif ">=" in input_list:
-            s = float(Fraction(input_list[1]))+0.05*float(Fraction(input_list[1]))
+            s = float(Fraction(input_list[1])) + \
+                0.05*float(Fraction(input_list[1]))
             s *= int(input_list[2])
-            return "Estimated quantity is {a:.2f} {b}".format(a = s, b = input_list[3])
+            return "Estimated quantity is {a:.2f} {b}".format(a=s, b=input_list[3])
 
         elif ">" in input_list:
-            s = float(Fraction(input_list[1]))+0.1*float(Fraction(input_list[1]))
+            s = float(Fraction(input_list[1])) + \
+                0.1*float(Fraction(input_list[1]))
             s *= int(input_list[2])
-            return "Estimated quantity is {a:.2f} {b}".format(a = s, b = input_list[3])            
-        
+            return "Estimated quantity is {a:.2f} {b}".format(a=s, b=input_list[3])
+
 
 input_one = ["< 1/4, 500 g", "~ 1/2, 400 g", ">= 3/4, 900 g"]
 input_two = "<= 1/4, 500 g"
