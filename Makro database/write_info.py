7f6ad4072d7af26d1670d1044db023230a502d83
Marlon Marín <marlonmariin97@outlook.com>
2023-02-12 17:28:18 -0500

    Now we have developed a code for converting our special codes into a readable form of avaialble quantity.

--- Makro database/write_info.py
+++ Makro database/write_info.py
@@ -1,21 +1,58 @@
+#!/usr/bin/env python3
+import re
+from fractions import Fraction
 
-def adds(string):
-    return str(string.split()) + "\n"
+try:
+    def get_ranges_string(input_string):
         
-    
+        input_list = input_string.replace(",", "").split()
 
+        for s in input_list:
 
-def write_data(filepath='./files/data.txt'):
-    with open(filepath, 'r', encoding = 'utf-8') as f:
-        lines = f.readlines()
+            if "<" in input_list:
+                s = float(Fraction(input_list[1])) - \
+                    0.1*float(Fraction(input_list[1]))
+                s *= float((input_list[2]))
+                return "{a:.2f} {b}\n".format(a=s, b=input_list[3])
 
-    with open(filepath, "w", encoding = 'utf-8') as file:
-        for line in lines:
-            modify_line = adds(line)
-            file.write(modify_line)
+            elif "<=" in input_list:
+                s = float(Fraction(input_list[1])) - \
+                    0.05*float(Fraction(input_list[1]))
+                s *= float((input_list[2]))
+                return "{a:.2f} {b}\n".format(a=s, b=input_list[3])
+
+            elif "~" in input_list:
+                s = float(Fraction(input_list[1])) - \
+                    0.01*float(Fraction(input_list[1]))
+                s *= float((input_list[2]))
+                return "{a:.2f} {b}\n".format(a=s, b=input_list[3])
+
+            elif ">=" in input_list:
+                s = float(Fraction(input_list[1])) + \
+                    0.05*float(Fraction(input_list[1]))
+                s *= float((input_list[2]))
+                return "{a:.2f} {b}\n".format(a=s, b=input_list[3])
+
+            elif ">" in input_list:
+                s = float(Fraction(input_list[1])) + \
+                    0.1*float(Fraction(input_list[1]))
+                s *= float((input_list[2]))
+                return "{a:.2f} {b}\n".format(a=s, b=input_list[3])
             
-    return 'Changes are done'
+            else:
+                return input_string
+            
+except TypeError:
+    print('Enter a valid input')
 
-overwrite_data = write_data(filepath= './files/data.txt')
-print(overwrite_data)
-
+def open_data(filepath):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    
+    with open(filepath, 'w', encoding = 'utf-8') as w:
+        for row in lines:
+            transformed_line = get_ranges_string(row)
+            w.write(transformed_line)
+        print("Written file")
+        
+open_data("./files/data.txt")
