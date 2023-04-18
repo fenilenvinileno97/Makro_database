86466dda69174a35110fc51f5710292af9c29085
Marlon Marín <marlonmariin97@outlook.com>
2023-04-17 21:22:36 -0500

    Python OS modules lessons

---
+++ Python OS/areas.py
@@ -0,0 +1,15 @@
+import math
+
+def triangle(base, height):
+	area = base*height/2
+	return area
+
+def rectangle(base, height):
+	area = base*height
+	return area
+
+def circle(radius):
+	area = math.pi*(radius**2)
+	return area
+
+
